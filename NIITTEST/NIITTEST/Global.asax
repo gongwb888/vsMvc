7288ea8750b578a5fecda7412e9a83a58be10ef8
gongwb888 <gongwb@126.com>
2016-07-31 10:16:51 +0800

    XX

---
+++ NIITTEST/NIITTEST/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NIITTEST.MvcApplication" Language="C#" %>
---
+++ NIITTEST/NIITTEST/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NIITTEST.Startup))]
+namespace NIITTEST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
